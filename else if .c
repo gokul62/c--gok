54caad72f6bb9691002759f880d6ea75ab2105c1
gokul62 <gokul6262lap@gmail.com>
2025-08-13 10:58:21 +0530

    else if .c

---
+++ else if .c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main()
+
+{ 
+    int a =4;
+    int b = 5;
+    
+    if(a==b)
+    
+    {
+        printf("a is = to b");
+        
+    }
+    
+    else if (a<b)
+{
+    printf("a is less than b");
+    
+}
+
+else 
+{
+    printf("no change");
+    
+}
+
+}

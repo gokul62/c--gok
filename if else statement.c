c6825e063ff070ddc82df60ea12145f3c05c085f
gokul62 <gokul6262lap@gmail.com>
2025-08-13 10:52:27 +0530

    Create if else statement.c

---
+++ if else statement.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*void main()
+
+{
+  if(5<2)
+  {
+      printf("true");
+  }
+      else 
+    {  printf("false");
+      
+  }
+}*/
+
+
+void main()
+
+{
+    int a= 3;
+    int b= 5;
+    
+    if(a!=b)
+    {
+        printf("true");
+    }
+    
+    else 
+    {
+        printf("false");
+    }
+    
+}
+  

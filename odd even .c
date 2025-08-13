2388892fe97bd34e64a7aa57cada7f237fb0fca1
gokul62 <gokul6262lap@gmail.com>
2025-08-13 11:38:17 +0530

    odd even .c

---
+++ odd even .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+
+{ 
+int num;
+printf("enter the num");
+scanf("%d",&num);
+
+if(num%2 == 0)
+{
+printf("even");
+}
+else 
+{
+printf("odd");
+}
+}

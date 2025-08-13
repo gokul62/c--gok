fdd1e302d369b5b4b526cfb0911b3f318b1c6ae8
gokul62 <gokul6262lap@gmail.com>
2025-08-13 11:17:22 +0530

    nested if.c

---
+++ nested if.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+    int a =4;
+    int b =-1;
+    
+    if(a>b)
+    {
+        if(b==0)
+        {
+            printf("b");
+        }
+        else 
+        {
+            printf("0");
+        }
+    }
+    else
+    {
+        printf(" not a prob");
+    }
+}

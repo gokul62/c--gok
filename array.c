5fbf88a9467a2e23a386586081f329ddec5ecf9c
gokul62 <gokul6262lap@gmail.com>
2025-08-15 14:00:11 +0530

    array.c

---
+++ array.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{ 
+   int heights[50]; // total size is 50
+   
+   heights[0] = 10;
+   heights[1] = 20;
+   heights[3] = 30;
+   
+   printf("%d",heights[0]);
+   printf("%d",heights[1]);
+   printf("%d",heights[3]);
+   
+   getch();
+   
+} 
+
+/*  // using for loop
+   heights[0] = 10;
+   heights[1] = 20;
+   heights[3] = 30;
+   
+  for(i=0;i<3;i++)
+  {
+   printf("%d \n",heights[i]);
+  }
+   getch();
+   */
+   
+   
+   /*
+    // using for loop
+   heights[0] = 10;
+   heights[1] = 20;
+   heights[3] = 30;
+   
+   for(i=3;i<6;i++) // geting input from user
+   {
+       print("%d \n",&heights[i])
+   }
+   
+  for(i=0;i<3;i++)
+  {
+   printf("%d \n",heights[i]);
+  }
+   getch();
+   */
+   
+   

10bcde6f2938d4cbad0fe5dfda04eeb955c4165b
gokul62 <gokul6262lap@gmail.com>
2025-08-16 08:56:38 +0530

    2d array.c

--- 2d array.c
+++ 2d array.c
@@ -1,1 +1,29 @@
 
+// str cat 
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+    int arr [2][2];
+    int i,j;
+    
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf( "enter the element ");
+           scanf("%d",&arr[i][j]);
+        }
+    }
+    
+    for(i=0;i<2;i++)
+    {
+        for (j=0;j<2;j++)
+        {
+            printf("%d",arr[i][j]);
+        }
+    }
+}

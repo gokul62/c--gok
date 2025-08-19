14e4eceb95984cce807a63fa705a3d5f97b20ccc
gokul62 <gokul6262lap@gmail.com>
2025-08-19 10:31:08 +0530

    recursion.c

---
+++ recursion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int fact(int n)
+{
+if(n <= 1)
+return 1;
+
+return n*fact(n-1);
+
+}
+
+void main()
+
+{
+
+int n = 5;
+int c = fact(n);
+printf("%d",c);
+
+}
+

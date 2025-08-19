6b56d83855e93d6e89b94b870fff0d7059b35b3f
gokul62 <gokul6262lap@gmail.com>
2025-08-19 10:01:24 +0530

    function.c

---
+++ function.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+int add()
+
+{
+    int a=2;
+    int b=3;
+    int c= a+b;
+    
+    return c;
+    
+}
+
+void main()
+{
+    printf("%d", add());//int ans = add();printf(%d",ans)
+    getch();
+    
+}

e270ee93bb0591a19ea81b816fc8143626c5b936
gokul62 <gokul6262lap@gmail.com>
2025-08-19 09:48:23 +0530

    transpose.c

---
+++ transpose.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+    
+    int arr1[2][2],arr2[2][2];
+    int i,j;
+    
+    
+    for (i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            
+            printf("enter the element :");
+            scanf("%d",&arr1[i][j]);
+        }
+    }
+    
+    printf("next array \n");
+    
+    for (i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            arr2[i][j] = arr1[j][i];
+        }
+    }
+        
+        for (i=0;i<2;i++)
+        {
+            for(j=0;j<2;j++)
+            {
+                printf("%d",arr2[i][j]);
+            }
+            printf("\n");
+        }
+        
+    }

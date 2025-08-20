b867f0cfc44d3f9b82bc91a8f9e7e30863cf939b
gokul62 <gokul6262lap@gmail.com>
2025-08-20 09:37:16 +0530

    factorial using recursive.c

--- factorial using recursive.c
+++ factorial using recursive.c
@@ -1,18 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
 int fact(int n)
+
 {
-    if (n <=1)   // base case
+    if(n == 0 || n == 1)
+    {
         return 1;
-    else
-        return n * fact(n - 1);  // recursive step
+    }
+    else 
+    {
+        return n*fact(n-1);
+    }
 }
 
-int main()
+void main()
+
 {
     int n;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-
-    printf("Factorial of %d is: %d\n", n, fact(n));
-
-    return 0;
+    printf("Enter the n value :");
+    scanf("%d",&n);
+    
+    printf("the factorial value of  n is :%d\n",n);
+    printf("the value of fact(n) is : %d",fact(n));
 }
